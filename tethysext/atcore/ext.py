4857c9e21ac3a440f03507ee85e3fe03ab3236cc
nswain <nswain@aquaveo.com>
2018-03-05 12:19:37 -0700

    Initial commit

---
+++ tethysext/atcore/ext.py
@@ -0,0 +1,12 @@
+from tethys_sdk.base import TethysExtensionBase
+
+
+class Atcore(TethysExtensionBase):
+    """
+    Tethys app class for atcore.
+    """
+
+    name = 'atcore'
+    package = 'atcore'
+    root_url = 'atcore'
+    description = 'Place a brief description of your app here.'
