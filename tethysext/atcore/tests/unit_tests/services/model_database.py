6819526d4f3a43c89f6f51ca2dcd57e56f58c527
teva <tveluppillai@aquaveo.com>
2018-06-18 12:01:50 -0600

    added tests for model database.

--- tethysext/atcore/tests/unit_tests/services/model_database.py
+++ tethysext/atcore/tests/unit_tests/services/model_database.py
@@ -8,15 +8,18 @@
 """
 import unittest
 import mock
+import sqlalchemy
 from tethys_sdk.base import TethysAppBase
 from tethysext.atcore.services.model_database import ModelDatabase
+from tethysext.atcore.services.model_database_connection import ModelDatabaseConnection
 
 
-CONN_1 = 'conn_1'
-CONN_2 = 'conn_2'
-CONN_3 = 'conn_3'
-CONN_4 = 'conn_4'
-CONN_5 = 'conn_5'
+CONN_1 = 'conn_1'  #: Count 5 size 10
+CONN_2 = 'conn_2'  #: Count 3 size 15
+CONN_3 = 'conn_3'  #: Count 5 size 15
+CONN_4 = 'conn_4'  #: Count 3 size 15
+CONN_5 = 'conn_5'  #: Unassigned connection
+CONN_6 = 'conn_6'  #: Get size connection
 
 
 class MockDeclarativeBase(object):
@@ -27,11 +30,27 @@ class MockDeclarativeBase(object):
         def create_all(self, engine):
             pass
 
+
+class FetchOne(object):
+
+    def __init__(self, size):
+        self.size = size
+
+
 class MockResponse(object):
 
     def __init__(self, count=None, size=None):
         self.count = count
         self.size = size
+
+    def scalar(self):
+        return self.size
+
+    def fetchone(self):
+        return FetchOne(self.size)
+
+    def close(self):
+        pass
 
 
 class MockEngine(object):
@@ -48,11 +67,12 @@ class MockEngine(object):
         CONN_1 and CONN_2 return different count and size.
         CONN_1 and CONN_3 return the same count, but different sizes.
         CONN_2 and CONN_4 return the same count and size.
+        CONN_6 return size in shape expected by get_size method.
         Args:
-            query:
+            query(str): the sql query.
 
         Returns:
-
+            list<MockResponse> or MockResponse: size or count in appropriate container.
         """
         if 'count' in query:
             if self.connection_name == CONN_1:
@@ -79,6 +99,9 @@ class MockEngine(object):
 
             if self.connection_name == CONN_4:
                 return [MockResponse(size=15)]
+
+            if self.connection_name == CONN_6:
+                return MockResponse(size=75)
 
 
 def mock_get_engine(connection_name, as_url=False):
@@ -121,28 +144,78 @@ class ModelDatabaseTests(unittest.TestCase):
         self.assertEqual('{}_{}'.format('foo', self.database_id), result)
 
     def test_get_id(self):
-        pass
+        md = ModelDatabase(self.mock_app, self.database_id)
+        result = md.get_id()
+        self.assertEqual('{}_{}'.format('foo', self.database_id), result)
 
-    def test_get_size(self):
-        pass
+    @mock.patch('tethysext.atcore.services.model_database.ModelDatabaseConnection')
+    def test_get_size(self, mock_mdc):
+        mmdc = mock_mdc.return_value
+        mmdc.get_engine.return_value = MockEngine(CONN_6)
+        md = ModelDatabase(self.mock_app)
+        result = md.get_size()
+        self.assertEquals(75, result)
+
+    @mock.patch('tethysext.atcore.services.model_database.ModelDatabaseConnection')
+    def test_get_size_pretty(self, mock_mdc):
+        mmdc = mock_mdc.return_value
+        mmdc.get_engine.return_value = MockEngine(CONN_6)
+        md = ModelDatabase(self.mock_app)
+        result = md.get_size(True)
+        self.assertEquals(75, result)
 
     def test_db_url(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.db_url
+        self.assertEquals(md.db_url, result)
 
     def test_db_url_obj(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.db_url_obj
+        self.assertEquals(md.db_url_obj, result)
 
     def test_model_db_connection(self):
-        pass
+        fake_url = 'postgresql://name:pass@localhost:5435/foo_239407239480712394'
+        self.mock_app.get_persistent_store_database = mock.MagicMock(
+            return_value=fake_url
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.model_db_connection
+        self.assertIsInstance(result, ModelDatabaseConnection)
+        self.assertEqual(fake_url, result.db_url)
 
     def test_get_engine(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.get_engine()
+        self.assertIsInstance(result, sqlalchemy.engine.Engine)
+        self.assertEqual(str(md.db_url_obj), str(result.url))
+        result.dispose()
 
     def test_get_session(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.get_session()
+        self.assertIsInstance(result, sqlalchemy.orm.session.Session)
+        result.close()
 
     def test_get_session_maker(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.get_session_maker()
+        self.assertIsInstance(result, sqlalchemy.orm.session.sessionmaker)
 
     def test_initialize_no_connections(self):
         self.mock_app.list_persistent_store_connections = mock.MagicMock(
@@ -243,10 +316,28 @@ class ModelDatabaseTests(unittest.TestCase):
         self.assertEqual(CONN_4, result)
 
     def test_exists(self):
-        pass
+        self.mock_app.persistent_store_exists = mock.MagicMock(
+            return_value=True
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.exists()
+        self.mock_app.persistent_store_exists.assert_called_with(md.database_id)
+        self.assertTrue(result)
 
     def test_list(self):
-        pass
+        databases = ['foo']
+        self.mock_app.list_persistent_store_databases = mock.MagicMock(
+            return_value=databases
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.list()
+        self.mock_app.list_persistent_store_databases.assert_called()
+        self.assertEquals(databases, result)
 
     def test_generate_id(self):
-        pass
+        self.mock_app.list_persistent_store_connections = mock.MagicMock(
+            return_value=[CONN_1]
+        )
+        md = ModelDatabase(self.mock_app)
+        result = md.generate_id()
+        self.assertIsNotNone(result)
