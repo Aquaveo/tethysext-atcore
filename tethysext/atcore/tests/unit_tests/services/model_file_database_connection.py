b5a2bd5af51f45a5ba644bedba5bf1b11cc92fd7
Corey Krewson <ckrewson@aquaveo.local>
2019-01-18 15:45:14 -0700

    fixed add_dir test for model_file_db_connection

--- tethysext/atcore/tests/unit_tests/services/model_file_database_connection.py
+++ tethysext/atcore/tests/unit_tests/services/model_file_database_connection.py
@@ -88,7 +88,7 @@ class ModelFileDatabaseConnectionTests(unittest.TestCase):
         self.assertTrue(os.path.isfile(os.path.join(self.db_dir, 'test.txt')))
 
     def test_add_dir(self):
-        test_add = os.path.join(self.root, "test_dir")
+        test_add = os.path.join(self.db_dir, "test_dir")
         os.mkdir(test_add)
         self.mdc.add(test_add)
         self.assertTrue(os.path.isdir(os.path.join(self.db_dir, 'test_dir')))
