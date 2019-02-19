a07cc0989fee15a0d12465c1cd8ad0c7fac20bb3
nswain <nswain@aquaveo.com>
2019-02-19 09:28:34 -0700

    Update version and gitlab config to only build on tags and branches.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ resource_files += find_resource_files('tethysext/' + ext_package + '/resources')
 
 setup(
     name=release_package,
-    version='0.2.2',
+    version='0.3.0',
     description='',
     long_description='',
     keywords='',
