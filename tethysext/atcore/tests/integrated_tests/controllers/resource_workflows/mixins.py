284383f4c7a06227bec1a1ead289fae988d188d1
mlebaron <mlebaron@aquaveo.com>
2019-08-28 13:21:36 -0400

    Finished tests for mixins.py

--- tethysext/atcore/tests/integrated_tests/controllers/resource_workflows/mixins.py
+++ tethysext/atcore/tests/integrated_tests/controllers/resource_workflows/mixins.py
@@ -1,7 +1,7 @@
 """
 ********************************************************************************
 * Name: mixins.py
-* Author: Tanner
+* Author: Tanner, mlebaron
 * Created On: May 7, 2019
 * Copyright: (c) Aquaveo 2019
 ********************************************************************************
@@ -10,9 +10,26 @@ from unittest import mock
 import unittest
 from tethysext.atcore.models.app_users import ResourceWorkflow, ResourceWorkflowStep
 from tethysext.atcore.controllers.resource_workflows.mixins import WorkflowViewMixin
+from tethysext.atcore.controllers.resource_workflows.mixins import ResultViewMixin
+from tethysext.atcore.models.app_users.resource_workflow_result import ResourceWorkflowResult
+from tethysext.atcore.tests.utilities.sqlalchemy_helpers import SqlAlchemyTestCase
+from tethysext.atcore.tests.utilities.sqlalchemy_helpers import setup_module_for_sqlalchemy_tests, \
+    tear_down_module_for_sqlalchemy_tests
+
+
+def setUpModule():
+    setup_module_for_sqlalchemy_tests()
+
+
+def tearDownModule():
+    tear_down_module_for_sqlalchemy_tests()
 
 
 class ClassWithWorkflowMixin(WorkflowViewMixin):
+    pass
+
+
+class ClassWithResultViewMixin(ResultViewMixin):
     pass
 
 
@@ -78,3 +95,37 @@ class WorkflowViewMixinTests(unittest.TestCase):
         self.instance.get_sessionmaker().assert_called()
         self.assertEqual(mock_session.query().filter().one(), ret)
         mock_session.close.assert_called()
+
+
+class ResultViewMixinTests(SqlAlchemyTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.workflow = ResourceWorkflow()
+
+        self.session.add(self.workflow)
+        self.session.commit()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_get_resource_result_model(self):
+        ret = ClassWithResultViewMixin().get_resource_workflow_result_model()
+
+        self.assertEqual(ret, ResourceWorkflowResult)
+
+    @mock.patch('tethysext.atcore.controllers.app_users.mixins.AppUsersViewMixin.get_sessionmaker')
+    def test_get_result_no_session(self, mock_get_session):
+        request = mock.MagicMock()
+        query = mock.MagicMock()
+        xfilter = mock.MagicMock()
+        one = mock.MagicMock()
+
+        one.return_value = self.workflow
+        xfilter.return_value = one
+        query.return_value = xfilter
+
+        mock_get_session.return_value = query
+
+        ClassWithResultViewMixin().get_result(request, 'a')
