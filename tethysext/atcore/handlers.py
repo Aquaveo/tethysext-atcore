61c54caecda76e070909ae5e485ec806609b5051
msouff <msouffront@aquaveo.com>
2019-11-14 17:35:22 -0700

    bokeh handler + FormInputRWS refactor

---
+++ tethysext/atcore/handlers.py
@@ -0,0 +1,27 @@
+"""
+********************************************************************************
+* Name: handlers
+* Author: msouffront
+* Created On: Nov 12, 2019
+* Copyright: (c) Aquaveo 2019
+********************************************************************************
+"""
+from tethys_sdk.base import with_request
+from tethys_apps.utilities import get_active_app
+import panel as pn
+from tethysext.atcore.models.app_users import ResourceWorkflowStep
+
+
+@with_request
+def panel_rws_handler(document):
+    app = get_active_app(document.request, get_class=True)
+    Session = app.get_persistent_store_database('primary_db', as_sessionmaker=True)
+    session = Session()
+
+    current_step_id = document.request.url_route['kwargs']['step_id']
+    current_step = session.query(ResourceWorkflowStep).get(current_step_id)
+
+    param_class = current_step.options['param_class']
+    panel = pn.Row(param_class.param)
+
+    panel.server_doc(document)
