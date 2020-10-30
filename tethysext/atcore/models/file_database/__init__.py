f1f0fcc32d684e474986636b969cdb0ea87dca06
Gage Larsen <gagelarsen53@gmail.com>
2020-10-30 10:43:56 -0600

    Initial file additions for file database models.

---
+++ tethysext/atcore/models/file_database/__init__.py
@@ -0,0 +1,10 @@
+"""
+********************************************************************************
+* Name: file_database
+* Author: glarsen
+* Created On: October 30, 2020
+* Copyright: (c) Aquaveo 2020
+********************************************************************************
+"""
+from tethysext.atcore.models.file_database.file_collection import FileCollection  # noqa: F401, E501
+from tethysext.atcore.models.file_database.file_database import FileDatabase  # noqa: F401, E501
