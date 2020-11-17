eb6854b04ab46baabf71706156859598aebeffab
Gage Larsen <gagelarsen53@gmail.com>
2020-11-17 10:30:47 -0700

    add SpatialResource stub

---
+++ tethysext/atcore/models/app_users/spatial_resource.py
@@ -0,0 +1,15 @@
+"""
+********************************************************************************
+* Name: spatial_resource.py
+* Author: glarsen
+* Created On: November 17, 2020
+* Copyright: (c) Aquaveo 2020
+********************************************************************************
+"""
+from tethysext.atcore.models.app_users.resource import Resource
+
+__all__ = ['SpatialResource']
+
+
+class SpatialResource(Resource):
+    pass
