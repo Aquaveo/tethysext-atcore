76ca7b5c19589945dab48233c28f9f6d6f3e065c
Nathan Swain <swainn@users.noreply.github.com>
2020-11-16 23:39:56 -0700

    Lower coverage requirement

--- ci-test.sh
+++ ci-test.sh
@@ -12,11 +12,11 @@ coverage run -a --rcfile=ci-coverage.ini -m unittest -v tethysext.atcore.tests.u
 unittest_ret_val=$?
 
 echo "Running Intermediate Tests..."
-coverage run -a --rcfile=ci-coverage.ini $1 test -v 2 tethysext.atcore.tests.integrated_tests
+coverage run -a --rcfile=ci-coverage.ini "$1" test -v 2 tethysext.atcore.tests.integrated_tests
 intermediate_ret_val=$?
 
 # Minimum Required Coverage
-minimum_required_coverage=95
+minimum_required_coverage=80
 coverage report -m --skip-covered --fail-under $minimum_required_coverage
 coverage_ret_val=$?
 
