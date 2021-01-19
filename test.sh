0de2035c8c049a29abf63e71075af845d73730a1
Gage Larsen <gagelarsen53@gmail.com>
2021-01-19 10:00:42 -0700

    change to not be verbose

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ if [ ! -f "$1" ]; then
 fi
 rm -f .coverage
 echo "Running Unit Tests..."
-coverage run -a --rcfile=coverage.ini -m unittest -v tethysext.atcore.tests.unit_tests
+coverage run -a --rcfile=coverage.ini -m unittest tethysext.atcore.tests.unit_tests
 echo "Running Intermediate Tests..."
 coverage run -a --rcfile=coverage.ini "$1" test -v 1 tethysext.atcore.tests.integrated_tests
 echo "Combined Coverage Report..."
