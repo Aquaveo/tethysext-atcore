f89ecdfd2067c3a44ef0eeb97e82bb51b6599afc
swainn <nswain@aquaveo.com>
2021-10-26 09:16:53 -0600

    Add support for "panel" method on Parameterized classes in the Form Step with bokeh renderer. The panel method is used to define a custom layout for the parameterized class (e.g.: multiple views). Adds support for submitting Number typed param widgets (slider) is the Form Step with bokeh renderer.

--- tethysext/atcore/public/resource_workflows/panel_form_prep.js
+++ tethysext/atcore/public/resource_workflows/panel_form_prep.js
@@ -1,5 +1,6 @@
 $(function() {
    $('#workflow-form').on('submit', function(e) {
+       console.warn('WARNING: Values will only be submitted for the following widgets: select, input, and slider.');
        // Add names to select boxes
        $('#workflow-form .bk-input-group select').each(function() {
            $(this).attr('name', 'param-form-' + $(this).prev('label').text().toLowerCase().split(' ').join('_'));
@@ -7,6 +8,15 @@ $(function() {
        // Add names to input boxes
        $('#workflow-form .bk-input-group input').each(function() {
            $(this).attr('name', 'param-form-' + $(this).prev('label').text().toLowerCase().split(' ').join('_'));
+       });
+       // Add names to slider widgets
+       $('#workflow-form .bk-input-group .bk-slider-title').each(function() {
+           // e.g.: "Wave length: 11.1" -> ["Some widget", "11.1"]
+           [slider_title, slider_value] = $(this).text().split(': ');
+           // e.g.: "Some widget" -> "param-form-some_widget"
+           let slider_name = `param-form-${slider_title.toLowerCase().split(' ').join('_')}`;
+           // Insert a new input field b/c the slider widgets don't have an input associated with them.
+           $(this).after(`<input hidden name=${slider_name} value=${slider_value}>`);
        });
    });
 });
