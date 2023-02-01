4e7705822221cb054ed45361b0f657fa7196ae80
dgallup <dgallup@aquaveo.com>
2023-02-01 16:35:58 -0700

    Test fix

--- tethysext/atcore/tests/integrated_tests/controllers/resource_workflows/map_workflows/spatial_input_mwv_tests.py
+++ tethysext/atcore/tests/integrated_tests/controllers/resource_workflows/map_workflows/spatial_input_mwv_tests.py
@@ -562,7 +562,7 @@ class SpatialInputMwvTests(WorkflowViewTestCase):
         geojson = {
             'features': [
                 {'geometry': {'coordinates': [], 'type': 'some_type'}, 'properties': {}},
-                {'geometry': {'coordinates': [], 'type': ''}}
+                {'geometry': {'coordinates': [], 'type': 'some_type'}, 'properties': {}}
             ]
         }
 
