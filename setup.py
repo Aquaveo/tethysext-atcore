ce63ba1e84d61911137c3f4959be7052b53ec7a8
Nathan Swain <nswain@aquaveo.com>
2024-09-14 15:55:51 -0600

    1.12.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ resource_files += find_resource_files('tethysext/' + ext_package + '/resources')
 
 setup(
     name=release_package,
-    version='1.1.2',
+    version='1.12.1',
     description='',
     long_description='',
     keywords='',
