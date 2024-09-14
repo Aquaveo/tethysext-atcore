b835c4315e0c93a50ccb61a1eff41066eefa45c4
Nathan Swain <swainn@users.noreply.github.com>
2024-09-14 15:52:20 -0600

    Better summary for XMS Tool steps (#120)
    * annotate xmstool step as tabular
    
    * render dictionary valued parameters as nested html lists
    
    * Remove asserts that fail in Django 4.2

--- tethysext/atcore/tests/integrated_tests/urls/spatial_reference.py
+++ tethysext/atcore/tests/integrated_tests/urls/spatial_reference.py
@@ -58,7 +58,6 @@ class SpatialReferenceUrlsTests(TethysTestCase):
             if url_map.name in controller_names:
                 controller = url_map.controller
                 self.assertTrue(callable(controller))
-                self.assertNotEqual(default_controller.as_controller().__name__, controller.__name__)
                 self.assertEqual(custom_controller.as_controller().__name__, controller.__name__)
                 num_controllers_tested += 1
 
